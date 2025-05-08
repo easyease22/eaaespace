be815ba2d3ac8643b7c8ee28cf57c115e79c464c
easyease22 <samwuwfc@gmail.com>
2025-05-08 23:17:51 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-// index.js placeholder
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import EaseSpaceApp from './EaseSpaceApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <EaseSpaceApp />
+  </React.StrictMode>
+);
